ae33eb88db170558d01ebdc6ff1adce6984d3c52
Liam McCormick <ljmc2000@gmail.com>
2018-06-23 19:21:50 +0100

    added table into index.php

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from functions import *
+from foodtable import make_menu,menu2string
 
 #load the page
 pagestring=loadpage("index.html")
@@ -7,6 +8,7 @@ common_header=loadheader()
 
 #create the page the user wants to see
 pagestring=pagestring.replace("%COMMON_HEADER%",common_header)
+pagestring=pagestring.replace("%FOODTABLE%",  menu2string( make_menu() )  )
 
 #send processed page to user
 print("Content-Type: text/html\n")
