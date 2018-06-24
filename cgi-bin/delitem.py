921259fd2696ef527d43a51ee42d3d7c21d6bce9
Liam McCormick <ljmc2000@gmail.com>
2018-06-24 18:57:54 +0100

    user can now delete an item from the menu

---
+++ cgi-bin/delitem.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3.5
+#delete an item from the menu
+
+#external functions
+import cgi, os
+
+#internal functions
+from functions import is_admin,sendto
+from database_connection import database_connect
+
+#http vars
+GET=cgi.FieldStorage()
+menunumber=GET["menunumber"].value
+
+#check user is administrator
+if not is_admin():
+	sendto("/",message="Permission denied")
+	quit()
+
+#sql connection
+myconnection,mycursor=database_connect()
+
+#delete picture from storage
+getpic="select picture from food where (menunumber=?)"
+mycursor.execute(getpic,(menunumber,))
+filename,=mycursor.fetchone()
+try:
+	filename=filename.decode()
+	os.remove("food_images/"+filename)
+except FileNotFoundError:
+	continue
+
+#delete food from database
+delitem="delete from food where (menunumber=?)"
+mycursor.execute(delitem,(menunumber,))
+myconnection.commit()
+
+sendto("/cgi-bin/admin.py")
+
+#close database connection
+mycursor.close()
+myconnection.close()
