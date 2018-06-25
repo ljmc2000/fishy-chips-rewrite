15c245fed411e4fbe86e7391a347eb72e9bfbbf9
Liam McCormick <ljmc2000@gmail.com>
2018-06-25 20:39:07 +0100

    added total calculation to the checkout table

--- libs/checkout_funcs.py
+++ libs/checkout_funcs.py
@@ -15,18 +15,25 @@ def gen_item_table():
 
 	checkout_table_row=loadsubpage("checkout_table_row.html")
 	returnme=loadsubpage("checkout_table_head.html")
+	total=0
 
 	for menunumber, name, price in mycursor:
 		menunumber=str(menunumber)
 		if "food"+menunumber in SESSION:
+			inbasket=SESSION["food"+menunumber]
+			batch_price=price*int(SESSION["food"+menunumber])
+			total=total+batch_price
+
 			row=checkout_table_row
 			row=row.replace("%MENUNUMBER%",menunumber)
 			row=row.replace("%NAME%",name.decode() )
-			row=row.replace("%INBASKET%",SESSION["food"+menunumber])
-			row=row.replace("%PRICE%", "€%.2f" % price)
+			row=row.replace("%INBASKET%",inbasket)
+			row=row.replace("%BATCH_PRICE%", "€%.2f" % batch_price)
 			returnme=returnme+row
 
-	returnme=returnme+loadsubpage("checkout_table_tail.html")
+	tail=loadsubpage("checkout_table_tail.html")
+	tail=tail.replace("%TOTAL%","€%.2f" % total)
+	returnme=returnme+tail
 
 	mycursor.close()
 	myconnection.close()
