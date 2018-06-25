b58b11a2ae6b0133f4e7a588b77f9e9e3f940462
Liam McCormick <ljmc2000@gmail.com>
2018-06-25 20:19:37 +0100

    can now print a chekout table but some features are missing like price calcultation

--- cgi-bin/checkout.py
+++ cgi-bin/checkout.py
@@ -4,10 +4,12 @@
 #internal libs
 from session import *
 from functions import loadpage,loadheader,declare_http
+from checkout_funcs import *
 
 #generate page
 pagestring=loadpage("checkout.html")
 pagestring=pagestring.replace("%COMMON_HEADER%",loadheader())
+pagestring=pagestring.replace("%ITEMCART_TABLE%",gen_item_table())
 
 #send page to user
 declare_http()
