5c994f4cfa34f70b764fc2267597972ceaab990a
Liam McCormick <ljmc2000@gmail.com>
2018-06-25 20:41:15 +0100

    added slightly better comments

--- libs/checkout_funcs.py
+++ libs/checkout_funcs.py
@@ -14,16 +14,18 @@ def gen_item_table():
 	mycursor.execute(getitems)
 
 	checkout_table_row=loadsubpage("checkout_table_row.html")
-	returnme=loadsubpage("checkout_table_head.html")
-	total=0
+	returnme=loadsubpage("checkout_table_head.html")	#first line of table
+	total=0		#track basket total
 
 	for menunumber, name, price in mycursor:
 		menunumber=str(menunumber)
 		if "food"+menunumber in SESSION:
+			#calculate how many and how much
 			inbasket=SESSION["food"+menunumber]
 			batch_price=price*int(SESSION["food"+menunumber])
 			total=total+batch_price
 
+			#delimit table
 			row=checkout_table_row
 			row=row.replace("%MENUNUMBER%",menunumber)
 			row=row.replace("%NAME%",name.decode() )
@@ -31,8 +33,8 @@ def gen_item_table():
 			row=row.replace("%BATCH_PRICE%", "€%.2f" % batch_price)
 			returnme=returnme+row
 
-	tail=loadsubpage("checkout_table_tail.html")
-	tail=tail.replace("%TOTAL%","€%.2f" % total)
+	tail=loadsubpage("checkout_table_tail.html")	#last line of table
+	tail=tail.replace("%TOTAL%","€%.2f" % total)	#apply total
 	returnme=returnme+tail
 
 	mycursor.close()
