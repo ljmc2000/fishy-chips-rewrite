bb51d267dcb6b7ed84e0eca45e152a0695d71f6a
Liam Mc Cormick <ljmc2000@gmail.com>
2018-06-30 19:38:20 +0100

    added a change password script. at the moment it's not finished and only checks the original password

---
+++ cgi-bin/change-password.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3.5
+#a script to allow the user to change their password
+
+#internal libs
+from functions import load_cookies,sendto
+from database_connection import database_connect
+from verify_password import verify_password
+from classes import User
+
+#external libs
+import cgi
+from os import environ
+
+#get cookies
+COOKIES=load_cookies()
+
+#ensure user is logged in
+if COOKIES.get("Login_UID"):
+	user=User(COOKIES["Login_UID"].value)
+
+#get post data
+POST=cgi.FieldStorage()
+oldpwd=POST["oldpwd"].value
+newpwd1=POST["newpwd1"].value
+newpwd2=POST["newpwd2"].value
+
+#get old password from database
+myconnection,mycursor=database_connect()
+getoldpassword="select password from users where(username=?)"
+mycursor.execute(getoldpassword,(user.username,) )
+(hashedword,)=mycursor.fetchone()
+hashedword=hashedword.decode()
+
+#check old password
+
+
+if not verify_password(oldpwd,hashedword):
+	sendto(environ["HTTP_REFERER"],message="wrong original password")
+
