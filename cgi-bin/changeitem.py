ef9f2b572785a89b602bcfabf4dc54286208c3c9
Liam Mc Cormick <ljmc2000@gmail.com>
2018-07-01 13:38:23 +0100

    moditem.py now allows user to change everything but picture

--- cgi-bin/changeitem.py
+++ cgi-bin/changeitem.py
@@ -2,11 +2,12 @@
 #modify an item from the menu
 
 #internal libs
-from functions import is_admin
+from functions import is_admin,sendto
 from database_connection import database_connect
 
 #external libs
 import cgi
+from os import environ
 
 #ensure user is admin
 if not is_admin:
@@ -19,5 +20,14 @@ name=POST["name"].value
 description=POST["description"].value
 price=POST["price"].value
 
-print("Content-type: text/plain\n")
-print(menunumber,name,description,price)
+#update database
+update_item="update food set name=?,description=?,price=? where (menunumber=?)"
+myconnection,mycursor=database_connect()
+mycursor.execute(update_item,(name,description,price,menunumber))
+myconnection.commit()
+
+mycursor.close()
+myconnection.close()
+
+#return user to last page
+sendto(environ["HTTP_REFERER"],message="menu entry updated")
