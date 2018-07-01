b0dc2fe65814dc021ccaf9d7d623ba8eefbee5c9
Liam Mc Cormick <ljmc2000@gmail.com>
2018-07-01 13:10:47 +0100

    there now exists a page for modifying foods but it isn't yet functional

---
+++ cgi-bin/moditem.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3.5
+#edit menuentries
+
+#internal libs
+from functions import *
+from classes import loadfood
+
+#external libs
+import cgi
+
+#page variables
+COOKIES=load_cookies()
+GET=cgi.FieldStorage()
+
+#check user is admin
+if not is_admin():
+	sendto("/",message="Access Denied")
+	quit()
+
+#prepare the page for the client
+pagestring=loadpage("moditem.html")
+pagestring=pagestring.replace("%COMMON_HEADER%",loadheader() )
+menuitem=loadfood(GET["menunumber"].value)
+pagestring=menuitem.delimit(pagestring)
+
+#send page to user
+declare_http()
+print(pagestring)
