f50ebdfce19a971910a2ea8c77c76f448806dad5
Liam Mc Cormick <ljmc2000@gmail.com>
2018-07-07 13:52:49 +0100

    added empty ish file for confirm.py

---
+++ cgi-bin/confirm.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.5
+#confirm the users order at the checkout
+
+#internal libs
+from session import session_start
+from database_connection import database_connect
+from classes import User
+from functions import load_cookies
+
